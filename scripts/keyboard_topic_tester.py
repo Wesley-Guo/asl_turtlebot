21e25bef3363347a3e4a05f5d4c7c6d3dedbc426
athorneak13 <athorne@stanford.edu>
2019-03-12 21:02:47 -0700

    manual merge of important files from adam_puddle, integrated now contains updated state machine from supervisor.py

---
+++ scripts/keyboard_topic_tester.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+# Adapted from https://github.com/turtlebot/turtlebot/blob/kinetic/turtlebot_teleop/scripts/turtlebot_teleop_key
+
+# Copyright (c) 2011, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the Willow Garage, Inc. nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import rospy
+
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+from asl_turtlebot_project_team13.msg import DetectedObject, DetectedObjectList
+import numpy as np
+
+import sys, select, termios, tty
+
+msg = """
+Keyboard tester for simulated state machine transition
+---------------------------
+s : stop sign detected
+d : send new delivery request
+f : send simulated object list with one object
+g : send simulated object list with three objects
+
+
+CTRL-C to quit
+"""
+
+sim_obj_list = ['apple', 'banana', 'blue raspberry']
+sim_obj_dists = [1, 2, 1.5]
+sim_obj_thetas = [np.pi/3, 0, -np.pi/3]
+
+delivery_list = 'banana,apple'
+
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+def makeFakeDetected(id_num=0, name='fake_obj', dist=1.0, side_len=0.5, theta_offset=0):
+    fake_obj = DetectedObject()
+    fake_obj.id = id_num
+    fake_obj.name = name
+    fake_obj.confidence = 100.0
+    fake_obj.distance = dist
+    fake_obj.thetaleft = np.arcsin(side_len/dist) - theta_offset
+    fake_obj.thetaright = np.arcsin(side_len/dist) + theta_offset
+    fake_obj.corners = [-side_len,-side_len,side_len,side_len] # corners format = [ymin,xmin,ymax,xmax]
+    return fake_obj
+
+if __name__=="__main__":
+    settings = termios.tcgetattr(sys.stdin)
+    
+    rospy.init_node('keyboard_tester')
+    stop_pub = rospy.Publisher('/detector/stop_sign', DetectedObject, queue_size=5)
+    object_pub = rospy.Publisher('/detector/objects', DetectedObjectList, queue_size=5)
+    request_pub = rospy.Publisher('/delivery_request', String, queue_size=5)
+
+    try:
+        print(msg)
+        obj_count = 0
+        while(1):
+            key = getKey()
+            if key == 's':
+                obj_count+=1
+                fake_sign = makeFakeDetected(id_num=obj_count, name='fake_sign_'+str(obj_count))
+                stop_pub.publish(fake_sign)
+            elif key == 'd':
+                request_pub.publish(delivery_list)
+            elif key == 'f':
+                obj_count+=1
+                apple_name = 'apple'+str(obj_count)
+                fake_apple = makeFakeDetected(id_num=obj_count, name=apple_name)
+                fake_list = DetectedObjectList()
+                fake_list.objects.append(apple_name)
+                fake_list.ob_msgs.append(fake_apple)
+                object_pub.publish(fake_list)
+            elif key == 'g':
+                fake_list = DetectedObjectList()
+                for sim_name, sim_dist, sim_offset in zip(sim_obj_list, sim_obj_dists, sim_obj_thetas):
+                    obj_count+=1
+                    enum_sim_name = sim_name + str(obj_count)
+                    new_sim_obj = makeFakeDetected(id_num=obj_count, name=enum_sim_name, dist=sim_dist, theta_offset=sim_offset)
+                    fake_list.objects.append(enum_sim_name)
+                    fake_list.ob_msgs.append(new_sim_obj)
+                object_pub.publish(fake_list)
+            else:
+                if (key == '\x03'):
+                    break
+
+    except Exception as e:
+        print(e)
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
--- scripts/supervisor.py
+++ scripts/supervisor.py
@@ -4,10 +4,13 @@ import rospy
 from gazebo_msgs.msg import ModelStates
 from std_msgs.msg import Float32MultiArray, String
 from geometry_msgs.msg import Twist, PoseArray, Pose2D, PoseStamped
-from asl_turtlebot.msg import DetectedObject
+from asl_turtlebot.msg import DetectedObject, DetectedObjectList
 import tf
+import tf.msg
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
 import math
 from enum import Enum
+import numpy as np
 
 # if sim is True/using gazebo, therefore want to subscribe to /gazebo/model_states\
 # otherwise, they will use a TF lookup (hw2+)
@@ -30,6 +33,9 @@ STOP_MIN_DIST = .5
 # time taken to cross an intersection
 CROSSING_TIME = 3
 
+# time to autonomously explore
+CROSSING_TIME = 60
+
 # state machine modes, not all implemented
 class Mode(Enum):
     IDLE = 1
@@ -38,7 +44,7 @@ class Mode(Enum):
     CROSS = 4
     NAV = 5
     MANUAL = 6
-
+    EXPLORE = 7
 
 print "supervisor settings:\n"
 print "use_gazebo = %s\n" % use_gazebo
@@ -53,6 +59,7 @@ class Supervisor:
         self.y = 0
         self.theta = 0
         self.mode = Mode.IDLE
+        self.modeBeforeStop = None
         self.last_mode_printed = None
         self.trans_listener = tf.TransformListener()
         # command pose for controller
@@ -61,10 +68,22 @@ class Supervisor:
         self.nav_goal_publisher = rospy.Publisher('/cmd_nav', Pose2D, queue_size=10)
         # command vel (used for idling)
         self.cmd_vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.objects_dict = {}
+        self.delivery_requests = []
+        self.home_base = "elephant"
+        self.home_base_dict = {}
+        self.goal_list = []
 
         # subscribers
         # stop sign detector
         rospy.Subscriber('/detector/stop_sign', DetectedObject, self.stop_sign_detected_callback)
+        #food/house detector
+        # rospy.Subscriber('/detector/pizza', DetectedObject, self.pizza_detected_callback)
+        # rospy.Subscriber('/detector/broccoli', DetectedObject, self.broccoli_detected_callback)
+        # rospy.Subscriber('/detector/elephant', DetectedObject, self.elephant_detected_callback)
+        # rospy.Subscriber('/detector/giraffe', DetectedObject, self.giraffe_detected_callback)
+        # rospy.Subscriber('/delivery/getFood', Delivery, self.delivery_callback)
+        rospy.Subscriber('/detector/objects', DetectedObjectList, self.object_list_callback)
         # high-level navigation pose
         rospy.Subscriber('/nav_pose', Pose2D, self.nav_pose_callback)
         # if using gazebo, we have access to perfect state
@@ -72,6 +91,8 @@ class Supervisor:
             rospy.Subscriber('/gazebo/model_states', ModelStates, self.gazebo_callback)
         # we can subscribe to nav goal click
         rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.rviz_goal_callback)
+        # deliveries subscriber
+        rospy.Subscriber('/delivery_request', String, self.delivery_request_callback)
         
     def gazebo_callback(self, msg):
         pose = msg.pose[msg.name.index("turtlebot3_burger")]
@@ -118,10 +139,225 @@ class Supervisor:
 
         # distance of the stop sign
         dist = msg.distance
-
+        print("stop sign distance")
+		print(dist)
         # if close enough and in nav mode, stop
-        if dist > 0 and dist < STOP_MIN_DIST and self.mode == Mode.NAV:
+        if dist > 0 and dist < STOP_MIN_DIST and self.mode == Mode.NAV or self.mode == Mode.EXPLORE:
             self.init_stop_sign()
+
+    def object_list_callback(self, msg):
+        for i in range(len(msg.objects)):
+            add_to_dict(msg.objects[i], msg.ob_msgs[i])
+
+    def add_to_dict(object_name, object_msg):
+        dist = object_msg.distance
+        print( object_name + " distance")
+        print(dist)
+        theta_left = object_msg.thetaleft
+        theta_right = object_msg.thetaright
+        theta_mid = 0
+        if theta_left >= theta_right:
+            theta_mid = (theta_left+theta_right)/2
+        else:
+            theta_mid = (theta_left + (theta_right - 2*np.pi))/2
+
+        pt = PoseStamped()
+        pt.header.frame_id = '/camera'
+        pt.header.stamp = rospy.get_rostime()
+        pt.pose.position.x = dist*np.sin(theta_mid)
+        pt.pose.position.y = 0
+        pt.pose.position.z = dist*np.cos(theta_mid)
+        quat = tf.transformations.quaternion_from_euler(0,theta_mid,0)
+        pt.pose.orientation.x = quat[0]
+        pt.pose.orientation.y = quat[1]
+        pt.pose.orientation.z = quat[2]
+        pt.pose.orientation.w = quat[3]
+
+        object_map_pt = self.tf_listener.transformPose("/map", pt)
+
+        euler_angles = tf.transformations.euler_from_quaternion(object_map_pt.pose.orientation)
+
+        object_map_pose = Pose2D()
+        object_map_pose.x = object_map_pt.pose.position.x
+        object_map_pose.y = object_map_pose.pose.position.y
+        object_map_pose.theta = euler_angles[2]
+
+        if object_name in self.objects_dict:
+            prevCount = self.objects_dict[object_name][1]
+            prevPoint = self.objects_dict[object_name][0]
+            newPointVec = [((object_map_pose.x+(prevPoint.x*prevCount))/(prevCount+1)), 
+                            ((object_map_pose.y+(prevPoint.y*prevCount))/(prevCount+1)), 
+                                ((object_map_pose.theta+(prevPoint.theta*prevCount))/(prevCount+1))]
+            newPoint = Pose2D()
+            newPoint.x = newPointVec[0]
+            newPoint.y = newPointVec[1]
+            newPoint.theta = newPointVec[2]
+            self.objects_dict[object_name] = (newPoint, prevCount+ 1)
+        else:
+            self.objects_dict[object_name] = (object_map_pose, 1)
+        
+
+
+    #Adam edit - detector for food 1 - TODO: change to actual 
+    # def pizza_detected_callback(self, msg):
+    # 	dist = msg.distance
+    # 	print("pizza distance")
+    # 	print(dist)
+    # 	theta_left = msg.thetaleft
+    # 	theta_right = msg.thetaright
+    # 	theta_mid = 0
+    # 	if theta_left >= theta_right:
+    # 		theta_mid = (theta_left+theta_right)/2
+    # 	else:
+    # 		theta_mid = (theta_left + (theta_right - 2*np.pi))/2
+
+    # 	pt = PointStamped()
+    #     pt.header.frame_id = '/camera'
+    #     pt.header.stamp = rospy.get_rostime()
+    #     pt.point.x = dist*np.sin(theta_mid)
+    #     pt.point.y = 0
+    #     pt.point.z = dist*np.cos(theta_mid)
+
+    # 	pizza_map_pt = self.tf_listener.transformPoint("/map", pt)
+
+    # 	if self.objects_dict["pizza"]:
+    # 		prevPoint = self.objects_dict["pizza"]
+    # 		newPointVec = [pizza_map_pt.point.x+prevPoint.point.x, pizza_map_pt.point.y + 
+    # 							prevPoint.point.y, pizza_map_pt.point.z + prevPoint.point.z]/2
+    # 		newPoint = PointStamped()
+    # 		newPoint.header.frame_id = '/camera'
+	   #      newPoint.header.stamp = rospy.get_rostime()
+	   #      newPoint.point.x = newPointVec[0]
+	   #      newPoint.point.y = newPointVec[1]
+	   #      newPoint.point.z = newPointVec[2]
+	   #      self.objects_dict["pizza"] = newPoint
+	   #  else:
+	   #  	self.objects_dict["pizza"] = pizza_map_pt
+
+
+    	#todo - store TF frame in dict
+
+    # def broccoli_detected_callback(self,msg):
+    # 	dist = msg.distance
+    # 	print("broccoli distance")
+    # 	print(dist)
+    # 	theta_left = msg.thetaleft
+    # 	theta_right = msg.thetaright
+    # 	theta_mid = 0
+    # 	if theta_left >= theta_right:
+    # 		theta_mid = (theta_left+theta_right)/2
+    # 	else:
+    # 		theta_mid = (theta_left + (theta_right - 2*np.pi))/2
+
+    # 	pt = PointStamped()
+    #     pt.header.frame_id = '/camera'
+    #     pt.header.stamp = rospy.get_rostime()
+    #     pt.point.x = dist*np.sin(theta_mid)
+    #     pt.point.y = 0
+    #     pt.point.z = dist*np.cos(theta_mid)
+
+    # 	broccoli_map_pt = self.tf_listener.transformPoint("/map", pt)
+
+    # 	if self.objects_dict["broccoli"]:
+    # 		prevPoint = self.objects_dict["broccoli"]
+    # 		newPointVec = [broccoli_map_pt.point.x+prevPoint.point.x, broccoli_map_pt.point.y + 
+    # 							prevPoint.point.y, broccoli_map_pt.point.z + prevPoint.point.z]/2
+    # 		newPoint = PointStamped()
+    # 		newPoint.header.frame_id = '/camera'
+	   #      newPoint.header.stamp = rospy.get_rostime()
+	   #      newPoint.point.x = newPointVec[0]
+	   #      newPoint.point.y = newPointVec[1]
+	   #      newPoint.point.z = newPointVec[2]
+	   #      self.objects_dict["broccoli"] = newPoint
+	   #  else:
+	   #  	self.objects_dict["broccoli"] = broccoli_map_pt
+
+
+
+    # 	#todo store TF frame in dict
+
+    # def elephant_detected_callback(self, msg):
+
+    # 	dist = msg.distance
+    # 	print("elephant distance")
+    # 	print(dist)
+    # 	theta_left = msg.thetaleft
+    # 	theta_right = msg.thetaright
+    # 	theta_mid = 0
+    # 	if theta_left >= theta_right:
+    # 		theta_mid = (theta_left+theta_right)/2
+    # 	else:
+    # 		theta_mid = (theta_left + (theta_right - 2*np.pi))/2
+
+    # 	pt = PointStamped()
+    #     pt.header.frame_id = '/camera'
+    #     pt.header.stamp = rospy.get_rostime()
+    #     pt.point.x = dist*np.sin(theta_mid)
+    #     pt.point.y = 0
+    #     pt.point.z = dist*np.cos(theta_mid)
+
+    # 	elephant_map_pt = self.tf_listener.transformPoint("/map", pt)
+
+    # 	if self.objects_dict["elephant"]:
+    # 		prevPoint = self.objects_dict["elephant"]
+    # 		newPointVec = [elephant_map_pt.point.x+prevPoint.point.x, elephant_map_pt.point.y + 
+    # 							prevPoint.point.y, elephant_map_pt.point.z + prevPoint.point.z]/2
+    # 		newPoint = PointStamped()
+    # 		newPoint.header.frame_id = '/camera'
+	   #      newPoint.header.stamp = rospy.get_rostime()
+	   #      newPoint.point.x = newPointVec[0]
+	   #      newPoint.point.y = newPointVec[1]
+	   #      newPoint.point.z = newPointVec[2]
+	   #      self.objects_dict["elephant"] = newPoint
+	   #  else:
+	   #  	self.objects_dict["elephant"] = elephant_map_pt
+
+
+    # 	#todo store TF frame in dict
+    # def giraffe_detected_callback(self,msg):
+    # 	dist = msg.distance
+    # 	print("giraffe distance")
+    # 	print(dist)
+    # 	theta_left = msg.thetaleft
+    # 	theta_right = msg.thetaright
+    # 	theta_mid = 0
+    # 	if theta_left >= theta_right:
+    # 		theta_mid = (theta_left+theta_right)/2
+    # 	else:
+    # 		theta_mid = (theta_left + (theta_right - 2*np.pi))/2
+
+    # 	pt = PointStamped()
+    #     pt.header.frame_id = '/camera'
+    #     pt.header.stamp = rospy.get_rostime()
+    #     pt.point.x = dist*np.sin(theta_mid)
+    #     pt.point.y = 0
+    #     pt.point.z = dist*np.cos(theta_mid)
+
+    # 	giraffe_map_pt = self.tf_listener.transformPoint("/map", pt)
+
+    # 	if self.objects_dict["giraffe"]:
+    # 		prevPoint = self.objects_dict["giraffe"]
+    # 		newPointVec = [giraffe_map_pt.point.x+prevPoint.point.x, giraffe_map_pt.point.y + 
+    # 							prevPoint.point.y, giraffe_map_pt.point.z + prevPoint.point.z]/2
+    # 		newPoint = PointStamped()
+    # 		newPoint.header.frame_id = '/camera'
+	   #      newPoint.header.stamp = rospy.get_rostime()
+	   #      newPoint.point.x = newPointVec[0]
+	   #      newPoint.point.y = newPointVec[1]
+	   #      newPoint.point.z = newPointVec[2]
+	   #      self.objects_dict["giraffe"] = newPoint
+	   #  else:
+	   #  	self.objects_dict["giraffe"] = giraffe_map_pt
+    	#todo store TF frame in dict
+
+
+    def delivery_request_callback(self,msg):
+        label_list = msg.split(',')
+        for label in label_list:
+            self.goal_list.append(self.objects_dict[label])
+        #reorder list to make optimal?
+        self.goal_list.append(self.objects_dict[self.home_base])
+        self.Mode = Mode.NAV
 
     def go_to_pose(self):
         """ sends the current desired pose to the pose controller """
@@ -158,7 +394,18 @@ class Supervisor:
         """ initiates a stop sign maneuver """
 
         self.stop_sign_start = rospy.get_rostime()
+        self.modeBeforeStop = self.mode
         self.mode = Mode.STOP
+
+    def init_explore_start(self):
+    	self.explore_start = rospy.get_rostime()
+    	self.mode = Mode.EXPLORE
+        #implement autonomous exploration
+        #self.nav_to_pose()
+
+    def has_explored(self):
+        """ checks if exploration is over """
+        return (self.mode == Mode.EXPLORE and (rospy.get_rostime()-self.explore_start)>rospy.Duration.from_sec(EXPLORE_TIME))
 
     def has_stopped(self):
         """ checks if stop sign maneuver is over """
@@ -173,7 +420,6 @@ class Supervisor:
 
     def has_crossed(self):
         """ checks if crossing maneuver is over """
-
         return (self.mode == Mode.CROSS and (rospy.get_rostime()-self.cross_start)>rospy.Duration.from_sec(CROSSING_TIME))
 
     def loop(self):
@@ -201,6 +447,7 @@ class Supervisor:
         if self.mode == Mode.IDLE:
             # send zero velocity
             self.stay_idle()
+            if
 
         elif self.mode == Mode.POSE:
             # moving towards a desired pose
@@ -219,15 +466,27 @@ class Supervisor:
         elif self.mode == Mode.CROSS:
             # crossing an intersection
             if self.has_crossed():
-                self.mode = Mode.NAV
+                self.mode = self.
             else:
                 self.nav_to_pose()
 
         elif self.mode == Mode.NAV:
-            if self.close_to(self.x_g,self.y_g,self.theta_g):
-                self.mode = Mode.IDLE
-            else:
+            while self.goal_list:
+                goal = goal_list[0]
+                self.x_g = goal.x
+                self.y_g = goal.y
+                self.theta_g = goal.theta
                 self.nav_to_pose()
+                if self.close_to(self.x_g,self.y_g,self.theta_g):
+                    del self.goal_list[0]
+            self.Mode = Mode.IDLE 
+
+        elif self.mode == Mode.EXPLORE:
+            self.init_explore_start()
+            if self.has_explored():
+                self.Mode = Mode.IDLE
+            else:
+                pass
 
         else:
             raise Exception('This mode is not supported: %s'
